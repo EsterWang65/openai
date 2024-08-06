7264e3ebc6f4b7a877d16bec92f393edfeadb9ea
EsterWang65 <150993639+EsterWang65@users.noreply.github.com>
2024-08-06 12:04:19 +0000

    0806

--- embedding-demo.py
+++ embedding-demo.py
@@ -1,5 +1,6 @@
 from openai import AzureOpenAI
 import numpy as np
+import pandas as pd
 
 client = AzureOpenAI(
   api_key = ("40dcb63620bb4875ad74e9878ad4c4cd"),  
@@ -11,10 +12,23 @@ def cosine_similarity(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 def generate_embeddings(text, model="text-embedding-ada-002"): # model = "deployment_name"
-    return np.array(client.embeddings.create(input = [text], model=model).data[0].embedding)
+    return client.embeddings.create(input = [text], model=model).data[0].embedding
 
-vector1 = generate_embeddings("coffee")
-vector2 = generate_embeddings("milk")
-vector3 = generate_embeddings("latte")
-cs = cosine_similarity(vector1 + vector2, vector3)
-print(cs)
+# vector1 = generate_embeddings("coffee")
+# vector2 = generate_embeddings("milk")
+# vector3 = generate_embeddings("latte")
+# cs = cosine_similarity(vector1 + vector2, vector3)
+# print(cs)
+
+# df = pd.read_csv("words.csv")
+# df["embedding"] = df["text"].apply(lambda x: generate_embeddings(x))
+# print(df)
+# df.to_csv("word_embeddings.csv")
+
+df = pd.read_csv("word_embeddings.csv")
+df["embedding"] = df["embedding"].apply(eval).apply(np.array)
+search = input("Please enter a term: ")
+search_vector = generate_embeddings(search)
+df["similarities"] = df["embedding"].apply(lambda x: cosine_similarity(x, search_vector))
+df_result = df.sort_values("similarities", ascending = False).head(10)
+print(df_result)
