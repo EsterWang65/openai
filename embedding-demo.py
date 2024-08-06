8cd5be26e3ed733f993ef03be0189426eb7ee6c6
EsterWang65 <150993639+EsterWang65@users.noreply.github.com>
2024-08-06 11:13:46 +0000

    0806

--- embedding-demo.py
+++ embedding-demo.py
@@ -1,21 +1,19 @@
-import os
-import re
-import requests
-import sys
-from num2words import num2words
-import os
-import pandas as pd
-import numpy as np
-import tiktoken
 from openai import AzureOpenAI
+import numpy as np
 
 client = AzureOpenAI(
-  api_key = os.getenv("40dcb63620bb4875ad74e9878ad4c4cd"),  
+  api_key = ("40dcb63620bb4875ad74e9878ad4c4cd"),  
   api_version = "2024-02-01",
-  azure_endpoint = os.getenv("https://gjun-openai-user04.openai.azure.com/")
+  azure_endpoint = ("https://gjun-openai-user04.openai.azure.com/")
 )
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 def generate_embeddings(text, model="text-embedding-ada-002"): # model = "deployment_name"
     return client.embeddings.create(input = [text], model=model).data[0].embedding
 
-#df_bills['ada_v2'] = df_bills["text"].apply(lambda x : generate_embeddings (x, model = 'text-embedding-ada-002')) # model should be set to the deployment name you chose when you deployed the text-embedding-ada-002 (Version 2) model
+vector1 = generate_embeddings("hello")
+vector2 = generate_embeddings("hi")
+cs = cosine_similarity(vector1, vector2)
+print(cs)
